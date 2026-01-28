bcc6512bdddb154688bfc0a010a786692ae2d405
GingerBunny <lukechalloner@gmail.com>
2026-01-28 15:32:15 +0000

    Add Javascript to deal with date rendering and pie chart view

--- static/scripts/sprints.js
+++ static/scripts/sprints.js
@@ -22,4 +22,103 @@ class addSprintDialog
     }
 }
 
+
+function renderSprints()
+{
+    function getdaysBetweenDates(date_start, date_end)
+    {
+        const diffTime = Math.abs(date_end - date_start);
+        return Math.floor(diffTime / (1000 * 60 * 60 * 24)); 
+    }
+
+    const sprints = document.querySelectorAll('div.sprint');
+    sprints.forEach(sprint => {
+
+        const date_start_element = sprint.querySelector('div.start-date');
+        const date_due_element = sprint.querySelector('div.due-date');
+        const day_txt_start = sprint.querySelector('div.start-days');
+        const day_txt_due = sprint.querySelector('div.due-days');
+
+        const date_start = new Date(date_start_element.dataset.date);
+        const date_due = new Date(date_due_element.dataset.date);
+        const date_now = new Date();
+
+        const diff_start = getdaysBetweenDates(date_start, date_now);
+        const diff_due = getdaysBetweenDates(date_now, date_due);
+
+        const sprint_has_started = (date_start.getTime() < date_now.getTime());
+        const sprint_has_ended = (date_due.getTime() < date_now.getTime());
+
+        day_txt_start.innerText = sprint_has_started ? "Started " + diff_start + " days ago" : "Starts in " + diff_start + " days";
+        day_txt_due.innerText = sprint_has_ended ? "Ended " + diff_due + " days ago" :  "Ends in " + diff_due + " days";
+
+        let style_day_txt_start = 'normal';
+        let style_day_txt_due = 'normal';
+        if (sprint_has_started)
+        {
+            if (sprint_has_ended)
+            {
+                style_day_txt_due = "warning";
+            }
+            else
+            {
+                if (diff_due <= 3) {style_day_txt_due = "warning";}
+                else if (diff_due <= 7) {style_day_txt_due = "caution";}
+            }
+        }
+        else
+        {
+            if (diff_start <= 3) {style_day_txt_start = "warning";}
+            else if (diff_start <= 7) {style_day_txt_start = "caution";}
+        }
+
+        day_txt_start.classList.add(style_day_txt_start);
+        day_txt_due.classList.add(style_day_txt_due);
+    });
+}
+
+function renderChart(chart_data)
+{
+    const ctx = document.getElementById('myChart');
+
+    if (ctx == undefined) {return false;}
+
+    new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            labels: ['TODO', 'WIP', 'DONE'],
+            datasets: [{
+                label: 'Tasks',
+                data: [chart_data.todo, chart_data.wip, chart_data.done],
+                backgroundColor: [
+                    '#af0000', // Red slice
+                    'orange', // Blue slice
+                    'green'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            plugins: {
+                legend: {
+                    display: false,
+                }
+            }
+        }
+    });
+
+    const total_el = document.getElementById('_chart_total');
+    total_el.innerText = chart_data.todo + chart_data.wip + chart_data.done;
+
+    const todo_legend = document.getElementById('_chart_legend_todo');
+    const wip_legend = document.getElementById('_chart_legend_wip');
+    const done_legend = document.getElementById('_chart_legend_done');
+
+    todo_legend.innerText = chart_data.todo + " tasks todo";
+    wip_legend.innerText = chart_data.wip + " tasks in progress";
+    done_legend.innerText = chart_data.done + " tasks completed";
+}
+
 new addSprintDialog();
+renderSprints();
+renderChart(chart_data);
